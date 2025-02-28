7c0fe0bde94fb62cfdbcecfde8cded515741fc34
Santosha2001 <santosha.xworkz@gmail.com>
2025-02-28 20:20:54 +0530

    coding questions

--- strings/src/string/ReverseString.java
+++ strings/src/string/ReverseString.java
@@ -6,7 +6,6 @@ public class ReverseString {
     public static void reverseString(String input) {
         String original = input;
         String reverse = "";
-
         for (int i = input.length() - 1; i >= 0; i--) {
             reverse = reverse + input.charAt(i);
         }
